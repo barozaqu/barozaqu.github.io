138c7af9bf1e992f92763e213e78e1fbb965b79b
leeganghee <78806331+barozaqu@users.noreply.github.com>
2021-06-19 17:59:03 +0900

    19일 마지막 작업 커밋
    slick slider 왜 안먹을까

--- js/script.js
+++ js/script.js
@@ -29,10 +29,32 @@
     $(function(){
 
       // 여기 할 거 집어넣으면 된다
-      
-        $('.your-class').slick({
-        //   setting-name: setting-value
-        });
-
-
+      $('.center').slick({
+        // setting-name: setting-value
+        // autoplay: true,
+        infinite: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
   });
