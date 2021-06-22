e203161af3115c8b76a460907f481cb0dfc7b8e6
barozaqu <barozaqu@gmail.com>
2021-06-22 14:27:22 +0900

    about 부분 dropdown 위치 수정
    수정 중

--- js/script.js
+++ js/script.js
@@ -23,11 +23,61 @@
       }
     ]
   }); */
-
   
-// $(document).ready(function(){
+  
+  // $(document).ready(function(){
     $(function(){
+      // <!-- 작업순서 -->
+      // <!-- 1. 초기 상태 -->
+      // <!-- 2. 실행 시 구현 -->
+    
+          // 초기 상태 세팅 : 안보이게
+          var $interviewItem = $('.interview__item');
+          var $skillsItem = $('.skills__item');
+          var $skillsThink = $('.skills__think');
+          
+          // 0 - > 1 
+          $skillsThink.hide();
+          $skillsItem.on('click', function(){
+            // console.log($(this));
+            // console.log($(this).siblings());
+            // siblings() 메서드는 형제 요소를 선택한다
+            // 태그, 아이디, 클래스명으로 요소 검색 가능
+            // () 없으면 전체 선택
+            
+            
+            
+            $skillsThink.stop().slideUp();
+            $(".skills__progressbar .skills__think").stop().slideToggle();
+            console.log($(this).children());
 
+                  // 셀렉터 this 다음에 어떻게!
+
+                  // $skillsIcon.toggleClass('visible');
+                  // 우측 아이콘 방향 돌아가는 액션
+              });
+          var $interviewItem = $('.interview__item');
+          var $interviewQuestion = $('.interview__question');
+          var $interviewAnswer = $('.interview__answer');
+          
+          // 0 - > 1 
+          $interviewAnswer.hide();
+              $interviewQuestion.on('click', function(){
+                  // console.log($(this));
+                  // console.log($(this).siblings());
+                  // siblings() 메서드는 형제 요소를 선택한다
+                  // 태그, 아이디, 클래스명으로 요소 검색 가능
+                  // () 없으면 전체 선택
+      
+                  $interviewAnswer.stop().slideUp();
+
+
+                  $(this).siblings().stop().slideToggle();
+                  // $skillsIcon.toggleClass('visible');
+                  // 우측 아이콘 방향 돌아가는 액션
+              });
+          
+      
       $('.test').slick({
       })
       // 여기 할 거 집어넣으면 된다
@@ -60,3 +110,4 @@
         ]
       }); */
   });
+
