ff2ec61b48871656fc7354663f2da079c0daf293
barozaqu <barozaqu@gmail.com>
2021-06-23 04:32:27 +0900

    수정 중.. contact 영역 QR
    맞던게 왜 틀려지고 font awesom icon은 왜 안나오는지

--- js/script.js
+++ js/script.js
@@ -23,65 +23,59 @@
       }
     ]
   }); */
-  
-  
-  // $(document).ready(function(){
-    $(function(){
-      // <!-- 작업순서 -->
-      // <!-- 1. 초기 상태 -->
-      // <!-- 2. 실행 시 구현 -->
-    
-          // 초기 상태 세팅 : 안보이게
-          var $interviewItem = $('.interview__item');
-          var $skillsItem = $('.skills__item');
-          var $skillsThink = $('.skills__think');
-          
-          // 0 - > 1 
-          $skillsThink.hide();
-          $skillsItem.on('click', function(){
-            // console.log($(this));
-            // console.log($(this).siblings());
-            // siblings() 메서드는 형제 요소를 선택한다
-            // 태그, 아이디, 클래스명으로 요소 검색 가능
-            // () 없으면 전체 선택
-            
-            
-            
-            $skillsThink.stop().slideUp();
-            $(".skills__progressbar .skills__think").stop().slideToggle();
-            console.log($(this).children());
 
-                  // 셀렉터 this 다음에 어떻게!
+// $(document).ready(function(){
+$(function () {
+  // <!-- 작업순서 -->
+  // <!-- 1. 초기 상태 -->
+  // <!-- 2. 실행 시 구현 -->
 
-                  // $skillsIcon.toggleClass('visible');
-                  // 우측 아이콘 방향 돌아가는 액션
-              });
-          var $interviewItem = $('.interview__item');
-          var $interviewQuestion = $('.interview__question');
-          var $interviewAnswer = $('.interview__answer');
-          
-          // 0 - > 1 
-          $interviewAnswer.hide();
-              $interviewQuestion.on('click', function(){
-                  // console.log($(this));
-                  // console.log($(this).siblings());
-                  // siblings() 메서드는 형제 요소를 선택한다
-                  // 태그, 아이디, 클래스명으로 요소 검색 가능
-                  // () 없으면 전체 선택
-      
-                  $interviewAnswer.stop().slideUp();
+  // 초기 상태 세팅 : 안보이게
+  var $interviewItem = $(".interview__item");
+  var $skillsItem = $(".skills__item");
+  var $skillsThink = $(".skills__think");
 
+  // 0 - > 1
+  $skillsThink.hide();
+  $skillsItem.on("click", function () {
+    // console.log($(this));
+    // console.log($(this).siblings());
+    // siblings() 메서드는 형제 요소를 선택한다
+    // 태그, 아이디, 클래스명으로 요소 검색 가능
+    // () 없으면 전체 선택
 
-                  $(this).siblings().stop().slideToggle();
-                  // $skillsIcon.toggleClass('visible');
-                  // 우측 아이콘 방향 돌아가는 액션
-              });
-          
-      
-      $('.test').slick({
-      })
-      // 여기 할 거 집어넣으면 된다
-     /*  $('.center').slick({
+    $skillsThink.stop().slideUp();
+    $(".skills__progressbar .skills__think").stop().slideToggle();
+    console.log($(this).children());
+
+    // 셀렉터 this 다음에 어떻게!
+
+    // $skillsIcon.toggleClass('visible');
+    // 우측 아이콘 방향 돌아가는 액션
+  });
+  var $interviewItem = $(".interview__item");
+  var $interviewQuestion = $(".interview__question");
+  var $interviewAnswer = $(".interview__answer");
+
+  // 0 - > 1
+  $interviewAnswer.hide();
+  $interviewQuestion.on("click", function () {
+    // console.log($(this));
+    // console.log($(this).siblings());
+    // siblings() 메서드는 형제 요소를 선택한다
+    // 태그, 아이디, 클래스명으로 요소 검색 가능
+    // () 없으면 전체 선택
+
+    $interviewAnswer.stop().slideUp();
+
+    $(this).siblings().stop().slideToggle();
+    // $skillsIcon.toggleClass('visible');
+    // 우측 아이콘 방향 돌아가는 액션
+  });
+
+  $(".test").slick({});
+  // 여기 할 거 집어넣으면 된다
+  /*  $('.center').slick({
         // setting-name: setting-value
         // autoplay: true,
         infinite: true,
@@ -109,5 +103,20 @@
           }
         ]
       }); */
-  });
+});
 
+/* ================= scroll up ================= */
+var portfolioDistance = $(".portfolio").offset().top;
+var $scrollUp = $(".scrollup");
+$(window).scroll(function () {
+  var scrollTop = $(this).scrollTop();
+  if (scrollTop > portfolioDistance) {
+    $scrollUp.addClass("visible");
+  } else {
+    $scrollUp.removeClass("visible");
+  }
+});
+$scrollUp.click(function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 });
+});
