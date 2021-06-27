10278a50893c0bfaaf644b59b1d1c8345bd6661f
barozaqu <barozaqu@gmail.com>
2021-06-28 02:22:48 +0900

    portfolio
    githun pull error

--- js/script.js
+++ js/script.js
@@ -24,99 +24,93 @@
     ]
   }); */
 
-// $(document).ready(function(){
-$(function () {
-  // <!-- 작업순서 -->
-  // <!-- 1. 초기 상태 -->
-  // <!-- 2. 실행 시 구현 -->
+ /* ================= skills  ================= */
+ $('.skills__header').click(function(){
+  $(this).children('.skill__icon--arrow').toggleClass('active');
+  $(this).siblings('.skill__data').stop().slideToggle();
+ });
+ 
+ 
+ /* ================= portfolio slider  ================= */
+ $('.portfolio__slider').slick({
+   dots:true,
+   autoplay: true,
+   autoplaySpeed: 2000,
+   infinite: true,
+ });
+ 
+ /* ================= portfolio modal  ================= */
+ $(function(){
+   // 이미지를 클릭했을때
+   var $modalImg = $('.modal__img');
+   var $portOverlay = $('.portfolio__overlay');
+   $modalImg.click(function(){
 
-  // 초기 상태 세팅 : 안보이게
-  var $interviewItem = $(".interview__item");
-  var $skillsItem = $(".skills__item");
-  var $skillsThink = $(".skills__think");
+     //   $("modalImg").css({
+     //     "top": (($(window).height()-$("modalImg").outerHeight())/2+$(window).scrollTop())+"px",
+     //     "left": (($(window).width()-$("modalImg").outerWidth())/2+$(window).scrollLeft())+"px"
+     //     //팝업창을 가운데로 띄우기 위해 현재 화면의 가운데 값과 스크롤 값을 계산하여 팝업창 CSS 설정
+     //  }); 
 
-  // 0 - > 1
-  $skillsThink.hide();
-  $skillsItem.on("click", function () {
-    // console.log($(this));
-    // console.log($(this).siblings());
-    // siblings() 메서드는 형제 요소를 선택한다
-    // 태그, 아이디, 클래스명으로 요소 검색 가능
-    // () 없으면 전체 선택
+       var src = $(this).children('img').attr('src');
+       var $portmodalImg = $('.portfolio__modal img');
+       console.log(src);
+       // modal에 있는 img 엘리먼트 src에 이미지 주소를 세팅
+       $portmodalImg.attr('src', src);
+       $portOverlay.show();
+   })
 
-    $skillsThink.stop().slideUp();
-    $(".skills__progressbar .skills__think").stop().slideToggle();
-    console.log($(this).children());
+   $portOverlay.click(function(e){
+       if(e.target == e.currentTarget){
+           $(this).fadeOut();
+       }
+   })
+})
 
-    // 셀렉터 this 다음에 어떻게!
+$(function(){
+ // 이미지를 클릭했을때
+ var $kakaoBtn = $('.kakao__btn');
+ var $kakaoImg = $('.kakao__img');
+ var $kakaoOverlay = $('.kakao__overlay');
+ $kakaoBtn.click(function(){
+   $kakaoOverlay.show();
+ });
+ $kakaoImg.click(function(){
 
-    // $skillsIcon.toggleClass('visible');
-    // 우측 아이콘 방향 돌아가는 액션
-  });
-  var $interviewItem = $(".interview__item");
-  var $interviewQuestion = $(".interview__question");
-  var $interviewAnswer = $(".interview__answer");
+   //   $("modalImg").css({
+   //     "top": (($(window).height()-$("modalImg").outerHeight())/2+$(window).scrollTop())+"px",
+   //     "left": (($(window).width()-$("modalImg").outerWidth())/2+$(window).scrollLeft())+"px"
+   //     //팝업창을 가운데로 띄우기 위해 현재 화면의 가운데 값과 스크롤 값을 계산하여 팝업창 CSS 설정
+   //  }); 
 
-  // 0 - > 1
-  $interviewAnswer.hide();
-  $interviewQuestion.on("click", function () {
-    // console.log($(this));
-    // console.log($(this).siblings());
-    // siblings() 메서드는 형제 요소를 선택한다
-    // 태그, 아이디, 클래스명으로 요소 검색 가능
-    // () 없으면 전체 선택
+     var src = $(this).children('img').attr('src');
+     var $kakaomodalImg = $('.kakao__modal img');
+     console.log(src);
+     // modal에 있는 img 엘리먼트 src에 이미지 주소를 세팅
+     $kakaomodalImg.attr('src', src);
+     $kakaoOverlay.show();
+ })
 
-    $interviewAnswer.stop().slideUp();
-
-    $(this).siblings().stop().slideToggle();
-    // $skillsIcon.toggleClass('visible');
-    // 우측 아이콘 방향 돌아가는 액션
-  });
-
-  $(".test").slick({});
-  // 여기 할 거 집어넣으면 된다
-  /*  $('.center').slick({
-        // setting-name: setting-value
-        // autoplay: true,
-        infinite: true,
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      }); */
-});
+ $kakaoOverlay.click(function(e){
+     if(e.target == e.currentTarget){
+         $(this).fadeOut();
+     }
+ })
+})
 
 /* ================= scroll up ================= */
 var portfolioDistance = $(".portfolio").offset().top;
 var $scrollUp = $(".scrollup");
 $(window).scroll(function () {
-  var scrollTop = $(this).scrollTop();
-  if (scrollTop > portfolioDistance) {
-    $scrollUp.addClass("visible");
-  } else {
-    $scrollUp.removeClass("visible");
-  }
+ var scrollTop = $(this).scrollTop();
+ if (scrollTop > portfolioDistance) {
+   $scrollUp.addClass("visible");
+ } else {
+   $scrollUp.removeClass("visible");
+ }
 });
 $scrollUp.click(function (e) {
-  e.preventDefault();
-  $("html, body").animate({ scrollTop: 0 });
+ e.preventDefault();
+ $("html, body").animate({ scrollTop: 0 });
 });
+ 
